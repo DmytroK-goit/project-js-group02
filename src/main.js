2564383b46f5ecb9bf6c885b8f44456d28c291c1
Kateryna Sukovaia <122564463+Katerynas82@users.noreply.github.com>
2024-08-20 20:09:41 +0100

    .

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-import '../src/hero'
+
+import './js/api.js';
+import './js/header.js';
+import './js/about-me.js';
+import './js/project.js';
+import './js/faq.js';
+import './js/covers.js';
+import './js/reviews.js';
+import './js/work-together.js';
+import '../src/hero';
+
